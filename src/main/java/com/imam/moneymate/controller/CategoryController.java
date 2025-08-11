30031d1433d7718f8cf095dd3e7aba1ff03e39c0
imam-007 <imimam8409@gmail.com>
2025-08-11 21:42:13 +0530

    implement create category and get category by current user

---
+++ src/main/java/com/imam/moneymate/controller/CategoryController.java
@@ -0,0 +1,32 @@
+package com.imam.moneymate.controller;
+
+import com.imam.moneymate.dto.CategoryDTO;
+import com.imam.moneymate.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO saveCategory = categoryService.saveCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(saveCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        List<CategoryDTO> categories = categoryService.getCategoriesForCurrentUser();
+
+        return ResponseEntity.ok(categories);
+    }
+}
---
+++ src/main/java/com/imam/moneymate/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.imam.moneymate.service;
+
+import com.imam.moneymate.dto.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+    public CategoryDTO saveCategory(CategoryDTO categoryDTO);
+
+    public List<CategoryDTO> getCategoriesForCurrentUser();
+}
