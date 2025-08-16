5c9c99a1711e31bc02355040688e9ad9ce099571
imam-007 <imimam8409@gmail.com>
2025-08-16 17:58:37 +0530

    implement signup frontend part

--- moneymatewebapp/src/pages/Signup.jsx
+++ moneymatewebapp/src/pages/Signup.jsx
@@ -1,5 +1,79 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../components/Input";
+
 const Signup = () => {
-  return <div>Sugnup</div>;
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-screen w-full relative flex items-center justify-center overflow-hidden">
+      <div className="relative z-10 w-full max-w-lg px-6">
+        <div className="bg-white bg-opacity-95 backdrop-blur-sm rounded-l-lg shadow-2xl p-8 max-h-[90vh] overflow-y-auto">
+          <h3 className="text-2xl font-semibold text-black text-center mb-2">
+            Create An account
+          </h3>
+          <p className="text-sm text-slate-700 text-center mb-8">
+            Start tracking your application
+          </p>
+          <form className="space-y-4">
+            <div className="flex justify-center mb-6">
+              {/**profile image */}
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+              <Input
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                label="Full Name"
+                placeholder="John Doe"
+                type="text"
+              />
+              <Input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                label="Eamil Address"
+                placeholder="xyz@gmail.com"
+                type="text"
+              />
+              <div className="col-span-2">
+                <Input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  label="Password"
+                  placeholder="******"
+                  type="password"
+                />
+              </div>
+            </div>
+            {error && (
+              <p className="text-red-800 text-sm text-center bg-red-50 p-2 rounded">
+                {error}
+              </p>
+            )}
+            <button
+              className="w-full py-3 text-lg font-medium bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+              type="submit"
+            >
+              SIGN UP
+            </button>
+            <p className="text-sm text-slate-800 text-center mt-6">
+              Already have an account?{" "}
+              <Link
+                to="/login"
+                className="font-medium text-blue-600 underline hover:text-blue-800"
+              >
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
