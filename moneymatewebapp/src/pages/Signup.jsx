05a0027489769052965a868efd836e29bd612a07
imam-007 <imimam8409@gmail.com>
2025-08-16 16:29:17 +0530

    setting up react project

---
+++ moneymatewebapp/src/pages/Signup.jsx
@@ -0,0 +1,5 @@
+const Signup = () => {
+  return <div>Sugnup</div>;
+};
+
+export default Signup;
---
+++ moneymatewebapp/src/util/apiEnpoints.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "https://money-mate-demo.onrender.com/api/v1";
+
+export const API_ENDPOINTS = {
+  LOGIN: "/login",
+  REGISTER: "/register",
+};
