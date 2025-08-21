d48d9f29b4bc6ea4222f4fe19dab5833a5aac9d8
imam-007 <imimam8409@gmail.com>
2025-08-21 23:51:07 +0530

    resolved cors issue for frontend aur

--- src/main/java/com/imam/moneymate/config/CorsConfig.java
+++ src/main/java/com/imam/moneymate/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://keen-kataifi-1fe5f9.netlify.app")
+                        .allowedOrigins("https://remarkable-kashata-7052b0.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
