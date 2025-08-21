d486ca02dae2dde1aeab2a2a99244836b7ba655a
imam-007 <imimam8409@gmail.com>
2025-08-21 20:47:57 +0530

    Implement showing recent transaction module

--- moneymatewebapp/src/components/RecentTransaction.jsx
+++ moneymatewebapp/src/components/RecentTransaction.jsx
@@ -4,14 +4,20 @@ import moment from "moment";
 
 const RecentTransaction = ({ transactions, onMore }) => {
   return (
-    <div className="card">
+    <div className="bg-white shadow-md rounded-2xl p-5">
+      {/* Header */}
       <div className="flex items-center justify-between">
-        <h4 className="text-lg">Recent Transaction</h4>
-        <button className="card-btn" onClick={onMore}>
-          More <ArrowRight className="text-base" size={15} />
+        <h4 className="text-lg font-semibold">Recent Transaction</h4>
+        <button
+          className="flex items-center gap-1 text-blue-600 hover:text-blue-800 font-medium cursor-pointer"
+          onClick={onMore}
+        >
+          More <ArrowRight size={15} />
         </button>
       </div>
-      <div className="mt-6">
+
+      {/* Transactions */}
+      <div className="mt-6 space-y-4">
         {transactions?.slice(0, 5)?.map((item) => (
           <TransactionsInfoCard
             key={item.id}
